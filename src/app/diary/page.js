c381b472a560aad5086854454225ca7641aee160
Kadek Astri Yani <kadekastriyani@gmail.com>
2023-11-23 14:30:57 +0800

    dynamic style campus

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -2,25 +2,38 @@
 import "@styles/diary.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
 
 export default function Diary() {
   const [judul, setJudul] = useState([]);
   const [isiDiary, setIsiDiary] = useState([]);
+  const [isData, setData] = useState([])
 
   const endpointAPI = "https://6555c0f184b36e3a431e3f34.mockapi.io/diaryku";
 
   async function getDiary() {
-    const res = await axios.get(endpointAPI);
-    const data = res.data;
+    try {
+      const res = await axios.get(endpointAPI);
 
-    //ambil judul
-    const judul = data.map((item) => item.judul);
-    setJudul(judul);
+      //ambil data
+      const dataJSON = res.data;
+      console.log('dalam', dataJSON);
+      setData(dataJSON)
 
-    //ambil isi_diary
-    const isi_diary = data.map((item) => item.isi_diary);
-    setIsiDiary(isi_diary);
+
+      //ambil judul
+      const judul = dataJSON.map((item) => item.judul);
+      setJudul(judul);
+
+      //ambil isi_diary
+      const isi_diary = dataJSON.map((item) => item.isi_diary);
+      setIsiDiary(isi_diary);
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
+
 
   useEffect(() => {
     getDiary();
@@ -28,21 +41,26 @@ export default function Diary() {
 
   return (
     <div>
-      {judul.length > 0 ? (
+      {isData ? (judul.length > 0 ? (
         <ul>
           {judul.map((item, idx) => (
-              <li>
-                <div className="diary-container">
-                    <h1>{judul[idx]}</h1>
-                    <p className="p-diary">{isiDiary[idx]}</p>
-                </div>
-              </li>
-
+            <Link href={`/diary/${(item)}/${(isiDiary[idx])}`}>
+            <li key={idx}>           
+              <div className={`diary-container ${idx === judul.length - 1? 'last-item' :''}`}>
+                <h1>{judul[idx]}</h1>
+                <p className="p-diary">{isiDiary[idx]}</p>
+              </div>
+            </li>
+            </Link>
           ))}
         </ul>
       ) : (
-        "API not loading"
-      )}
+        "API is loading"
+      )
+      ) : 'API-nya empty'
+
+      }
+
     </div>
   );
 }
