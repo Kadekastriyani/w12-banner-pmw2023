462f21fae844a35d7fd7129425672a4e08a9d682
Kadek Astri Yani <kadekastriyani@gmail.com>
2023-11-30 08:12:29 +0800

    Menambahkan POST METHOD

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -5,35 +5,86 @@ import axios from "axios";
 import Link from "next/link";
 
 export default function Diary() {
-  const [judul, setJudul] = useState([]);
-  const [isiDiary, setIsiDiary] = useState([]);
-  const [isData, setData] = useState([])
-
+  //GET
+  const [getJudul, setGetJudul] = useState([]);
+  const [getIsiDiary, setGetIsiDiary] = useState([]);
+  const [getKoleksiData, setGetKoleksiData] = useState([]);
   const endpointAPI = "https://6555c0f184b36e3a431e3f34.mockapi.io/diaryku";
-
   async function getDiary() {
     try {
       const res = await axios.get(endpointAPI);
 
       //ambil data
       const dataJSON = res.data;
-      console.log('dalam', dataJSON);
-      setData(dataJSON)
-
+      console.log("DATA DALAM", dataJSON);
+      setGetKoleksiData(dataJSON);
 
       //ambil judul
       const judul = dataJSON.map((item) => item.judul);
-      setJudul(judul);
+      console.log("JUDUL DALAM", judul);
+      setGetJudul(judul);
 
       //ambil isi_diary
       const isi_diary = dataJSON.map((item) => item.isi_diary);
-      setIsiDiary(isi_diary);
-
+      console.log("ISI DALAM", isi_diary);
+      setGetIsiDiary(isi_diary);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   }
 
+  //POST
+  const [postTulisJudul, setPostTulisJudul] = useState("");
+  const [postTulisDiary, setPostTulisDiary] = useState("");
+
+  async function postDiary() {
+    const updatedDiary = [
+      ...getKoleksiData,
+      { judul: postTulisJudul, isi_diary: postTulisDiary },
+    ];
+
+    setGetKoleksiData(updatedDiary);
+    setPostTulisJudul("");
+    setPostTulisDiary("");
+
+    try {
+      const res = await axios.post(endpointAPI, {
+        judul: postTulisJudul,
+        isi_diary: postTulisDiary,
+      });
+
+      if (res.status >= 200 && res.status < 300) {
+        getDiary();
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      alert("Failed to POST API: " + error);
+    }
+  }
+
+  function handlerInputJudul(event) {
+    setPostTulisJudul(event.target.value);
+  }
+
+  function handlerInputIsiDiary(event) {
+    setPostTulisDiary(event.target.value);
+  }
+
+  function handlerSubmitDiary(event) {
+    event.preventDefault();
+    if (postTulisJudul && postTulisDiary) {
+      postDiary();
+    } else {
+      alert("Isi judul dan diary terlebih dahulu!");
+    }
+  }
+
+  function handlerKeyEnter(e) {
+    if (e.key === "Enter") {
+      handlerSubmitDiary(e);
+    }
+  }
 
   useEffect(() => {
     getDiary();
@@ -41,26 +92,66 @@ export default function Diary() {
 
   return (
     <div>
-      {isData ? (judul.length > 0 ? (
-        <ul>
-          {judul.map((item, idx) => (
-            <Link href={`/diary/${(item)}/${(isiDiary[idx])}`}>
-            <li key={idx}>           
-              <div className={`diary-container ${idx === judul.length - 1? 'last-item' :''}`}>
-                <h1>{judul[idx]}</h1>
-                <p className="p-diary">{isiDiary[idx]}</p>
-              </div>
-            </li>
-            </Link>
-          ))}
-        </ul>
+      {/* POST DIARY */}
+      <div className="banner-container">
+        <div className="cta-banner-wrapper">
+          {/* Tombol CTA */}
+          <input
+            name="input-judul"
+            type="text"
+            placeholder="Tuliskan judulmu.."
+            onChange={handlerInputJudul}
+            onKeyDown={handlerKeyEnter}
+            value={postTulisJudul}
+          />
+          <input
+            name="input-diary"
+            type="text"
+            placeholder="Tuliskan diarymu.."
+            onChange={handlerInputIsiDiary}
+            onKeyDown={handlerKeyEnter}
+            value={postTulisDiary}
+          />
+          {postTulisJudul && postTulisDiary ? (
+            <div className="cta-button" onClick={handlerSubmitDiary}>
+              <p>Submit Diary</p>
+            </div>
+          ) : (
+            <div
+              className="cta-button disabled"
+              onClick={() => alert("Isi terlebih dahulu!")}
+            >
+              <p>Disabled</p>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* MAP LIST DIARY */}
+      {getKoleksiData ? (
+        getJudul.length > 0 ? (
+          <ul>
+            {getJudul.map((item, idx) => (
+              <Link href={`/diary/${item}/${getIsiDiary[idx]}`} key={idx}>
+                <li>
+                  <div
+                    className={`diary-container ${
+                      idx === getJudul.length - 1 ? "last-item" : ""
+                    }`}
+                  >
+                    <h1>{getJudul[idx]}</h1>
+                    <p className="p-diary">{getIsiDiary[idx]}</p>
+                  </div>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          "API is loading"
+        )
       ) : (
-        "API is loading"
-      )
-      ) : 'API-nya empty'
-
-      }
-
+        "API-nya empty"
+      )}
     </div>
   );
 }
